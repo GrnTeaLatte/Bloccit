1ad78eb65f5afb0c5b43a53febbad73f011a60d5
Audrey Fu <audreyfu14@gmail.com>
2016-08-02 20:47:19 -0700

    Assignment 33 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-require 'random_data'
+include RandomData
 
 50.times do
-
   Post.create!(
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
@@ -9,12 +8,18 @@ require 'random_data'
 end
 posts = Post.all
 
-#Create comments
-
 100.times do
   Comment.create!(
     post: posts.sample,
     body: RandomData.random_paragraph
+  )
+end
+
+100.times do
+  Question.create!(
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    resolved: false
   )
 end
 
