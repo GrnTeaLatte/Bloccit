e60ce8d05b309dd46e786f26ac32598575db937c
Audrey Fu <audreyfu14@gmail.com>
2016-09-06 19:31:32 -0700

    Assignment 40 completed

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,6 +4,7 @@ class TopicsController < ApplicationController
 
   before_action :authorize_user, except: [:index, :show]
 
+
   def index
     @topics = Topic.all
   end
@@ -33,31 +34,31 @@ class TopicsController < ApplicationController
   end
 
 
-   def update
-     @topic = Topic.find(params[:id])
+  def update
+    @topic = Topic.find(params[:id])
 
-     @topic.assign_attributes(topic_params)
+    @topic.assign_attributes(topic_params)
 
-     if @topic.save
-        flash[:notice] = "Topic was updated successfully."
-       redirect_to @topic
-     else
-       flash.now[:alert] = "Error saving topic. Please try again."
-       render :edit
-     end
+    if @topic.save
+      flash[:notice] = "Topic was updated successfully."
+      redirect_to @topic
+    else
+      flash.now[:alert] = "Error saving topic. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+   @topic = Topic.find(params[:id])
+
+   if @topic.destroy
+     flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+     redirect_to action: :index
+   else
+     flash.now[:alert] = "There was an error deleting the topic."
+     render :show
    end
-
-   def destroy
-     @topic = Topic.find(params[:id])
-
-     if @topic.destroy
-       flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
-       redirect_to action: :index
-     else
-       flash.now[:alert] = "There was an error deleting the topic."
-       render :show
-     end
-   end
+  end
 
    private
 
@@ -66,8 +67,8 @@ class TopicsController < ApplicationController
    end
 
    def authorize_user
-     unless current_user.admin?
-       flash[:alert] = "You must be an admin to do that."
+     unless current_user.admin? || current_user.moderator? && params[:action] == "update"
+       flash[:alert] = "You must be an admin or moderator to do that."
        redirect_to topics_path
      end
    end
