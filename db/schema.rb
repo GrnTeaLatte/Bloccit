fb434f4541f446ff01543894dbe91c20bed5caa9
Audrey Fu <audreyfu14@gmail.com>
2016-10-09 23:04:23 -0700

    Checkpoint 46 interfaces: completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908021727) do
+ActiveRecord::Schema.define(version: 20161001223234) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
