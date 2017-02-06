7bd3f09be3312af16f869921052f9b5e3240483e
Audrey Fu <audreyfu14@gmail.com>
2017-02-06 10:09:16 -0800

    Javascript

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,9 @@ ActiveRecord::Schema.define(version: 20161021142234) do
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.integer  "post_id"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.integer  "commentable_id"
-    t.string   "commentable_type"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
